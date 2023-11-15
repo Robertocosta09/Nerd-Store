1250c824d2faed57450a74b6123a7f09a19ab05f
Roberto Costa <robertohsantos19@gmail.com>
2023-11-15 21:23:13 +0000

    Entrega2

---
+++ src/Components/navbar/index.tsx
@@ -0,0 +1,15 @@
+const NavBar = () => {
+
+    return (
+        <div className="bg-cyan-950 p-5	shadow-lg text-white flex justify-between">
+         <h1>Nerd Store</h1>
+             <ul className="flex gap-5">
+                <li>Home</li>
+                <li>Produtos</li>
+                <li>Login</li>
+             </ul>
+</div>
+    )
+}
+
+export default NavBar;
